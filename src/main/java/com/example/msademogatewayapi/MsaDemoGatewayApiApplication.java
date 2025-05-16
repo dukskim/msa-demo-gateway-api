5615b8f630b65b4a74cad81a4bda0dc1da3170ed
kdhm <test@test.com>
2025-05-16 18:40:53 +0900

    프로젝트생성

---
+++ src/main/java/com/example/msademogatewayapi/MsaDemoGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.msademogatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaDemoGatewayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaDemoGatewayApiApplication.class, args);
+    }
+
+}
